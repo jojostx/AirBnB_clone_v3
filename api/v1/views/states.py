080a4b2c6a9dec10a07f5497a5bd43912fd580cf
Ari Jay <ikuskid7@gmail.com>
2023-05-02 16:39:53 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,7 +16,7 @@ def states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def r_state_id(state_id):
     """ Retrieves a State object """
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if not state:
         abort(404)
     return jsonify(state.to_dict())
@@ -26,7 +26,7 @@ def r_state_id(state_id):
                  strict_slashes=False)
 def del_state(state_id):
     """ Deletes a State object """
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if not state:
         abort(404)
     state.delete()
@@ -51,7 +51,7 @@ def post_state():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """ Updates a State object """
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if not state:
         abort(404)
 
